229bf2fd024acabcba3d5fc6f7280595af6e3bda
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-02-14 09:54:14 -0600

    22.2.1 - Completed TitleGameState

--- GameStates/TitleGameState.cs
+++ GameStates/TitleGameState.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Engine;
+using Engine.UI;
+using Microsoft.Xna.Framework;
 
 namespace TickTickBoom.GameStates
 {
-    internal class TitleGameState
+    class TitleGameState : GameState
     {
+        #region Member Variables
+        Button playButton;
+        Button helpButton;
+        string TITLE_SCREEN_LOCATION = "Sprites/Backgrounds/spr_title";
+        string PLAY_BUTTON_LOCATION = "Sprites/UI/spr_button_play";
+        string HELP_BUTTON_LOCATION = "Sprites/UI/spr_button_help";
+
+        #endregion
+
+        #region Constructor
+        public TitleGameState()
+        {
+            // Load the title screen
+            SpriteGameObject titleScreen = new SpriteGameObject(TITLE_SCREEN_LOCATION, TickTickBoom.DEPTH_LAYER_BACKGROUND);
+            gameObjectList.AddChild(titleScreen);
+            // Add the play button
+            playButton = new Button(PLAY_BUTTON_LOCATION, TickTickBoom.DEPTH_LAYER_UI_FOREGROUND);
+            playButton.LocalPosition = new Vector2(600, 540);
+            // Add a help button
+            helpButton = new Button(HELP_BUTTON_LOCATION, TickTickBoom.DEPTH_LAYER_UI_FOREGROUND);
+            helpButton.LocalPosition = new Vector2(600, 600);
+            // Add buttons to game list
+            gameObjectList.AddChild(playButton);
+            gameObjectList.AddChild(helpButton);
+        }
+        #endregion
+
+        #region Public Methods
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            if (playButton.Pressed)
+            {
+                ExtendedGame.GameStateManager.SwitchGameState(ExtendedGameWithLevels.STATENAME_LEVELSELECT);
+            }
+            else if (helpButton.Pressed)
+            {
+                ExtendedGame.GameStateManager.SwitchGameState(ExtendedGameWithLevels.STATENAME_HELP);
+            }
+        }
+        #endregion
     }
 }
