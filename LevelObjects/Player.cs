e5fba54bf8fd197b1c7df9bb8b75de0fed23e93c
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-03-04 20:20:02 -0600

    Completed 24.1.1-24.1.3

--- LevelObjects/Player.cs
+++ LevelObjects/Player.cs
@@ -8,24 +8,29 @@ namespace TickTickBoom
     {
         #region Constants
         // Walking speed in pixels per second
-        const float WALKING_SPEED = 425.5f;
+
         const string IDLE_ANIMATION_LOCATION = "Sprites/LevelObjects/Player/spr_idle";
         const string RUN_ANIMATION_LOCATION = "Sprites/LevelObjects/Player/spr_run@13";
         const string JUMP_ANIMATION_LOCATION = "Sprites/LevelObjects/Player/spr_jump@14";
         const string CELEBRATION_ANIMATION_LOCATION = "Sprites/LevelObjects/Player/spr_celebrate@14";
         const string DIE_ANIMATION_LOCATION = "Sprites/LevelObjects/Player/spr_die@5";
         const string EXPLODE_ANIMATION_LOCATION = "Sprites/LevelObjects/Player/spr_explode@5x5";
+        const float WALKING_SPEED = 425.5f;
         const float IDLE_ANIAMTION_FRAMETIME = .1f;
         const float RUN_ANIMATION_FRAMETIME = .04f;
         const float JUMP_ANIMATION_FRAMETIME = .8f;
         const float CELEBRATE_ANIMATION_FRAMETIME = .05f;
         const float DIE_ANIMATION_FRAMETIME = .1f;
         const float EXPLODE_ANIMATION_FRAMETIME = .04f;
+
+        const float JUMP_SPEED = 900.0f;
+        const float GRAVITY = 2300.0f;
+        const float MAX_FALL_SPEED = 1200.0f;
         #endregion
         #region Member Variables
         // Flag to check if player is facing left or not
         bool isFacingLeft;
-
+        bool isGrounded;
 
         #endregion
         #region Constructor
@@ -65,11 +70,37 @@ namespace TickTickBoom
                 velocity.X = 0;
                 PlayAnimation("idle");
             }
+            // Allow for jumping
+            if (isGrounded && inputHelper.IsKeyPressed(Keys.Space))
+            {
+                velocity.Y = -JUMP_SPEED;
+                PlayAnimation("jump");
+            }
+            // Show the falling animation
+            if (!isGrounded)
+            {
+                int fallSpriteIndex = 8;
+                PlayAnimation("jump", false, fallSpriteIndex);
+            }
             SetOriginToBottomCenter();
             sprite.IsMirrored = isFacingLeft;
+
+        }
+        public void ApplyGravity(GameTime gameTime)
+        {
+            velocity.Y += GRAVITY * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (velocity.Y > MAX_FALL_SPEED)
+            {
+                velocity.Y = MAX_FALL_SPEED;
+            }
+
+        }
+        public override void Update(GameTime gameTime)
+        {
+            ApplyGravity(gameTime);
+            base.Update(gameTime);
         }
         #endregion
-
         #region Private Methods
         private void SetOriginToBottomCenter()
         {
